c341039e3f87d776f16029b8f2ff1d30a90372af
debarghya17 <bangadhipati@gmail.com>
2025-08-12 01:01:36 +0530

    solved some questions

---
+++ Hard/23. Merge k Sorted Lists/Solutions.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+        for (ListNode node : lists) {
+            if (node != null) pq.add(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            tail.next = node;
+            tail = tail.next;
+            if (node.next != null) pq.add(node.next);
+        }
+
+        return dummy.next;
+    }
+}
