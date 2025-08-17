a92b6d7c7bdcfcac40247153b47fe5169e848a1e
debarghya17 <bangadhipati@gmail.com>
2025-08-17 11:35:09 +0530

    solved a question

---
+++ Medium/837. New 21 Game/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) {
+            return 1.0; // Alice will always have <= n points
+        }
+
+        double[] dp = new double[n + 1]; 
+        dp[0] = 1.0;
+
+        double windowSum = 1.0; // sum of last maxPts dp values
+        double result = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i]; // still drawing cards
+            } else {
+                result += dp[i]; // stopped drawing
+            }
+
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts]; // slide window
+            }
+        }
+
+        return result;
+    }
+}
